89c677b076cbbf6b57e90511f8385cd0fd4cbc0f
Mbye Sallah <mbyesallah17@gmail.com>
2021-06-01 12:22:43 +0200

    new updates made

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -48,26 +48,6 @@ login_manager.init_app(app)
 
 
 #Create a user model
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    created = db.Column(db.DateTime, default=datetime.now, nullable=False)
-    firstname = db.Column(db.String(80), nullable=False)
-    lastname = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(120))
-    admin = db.Column(db.Boolean, nullable=False, default=False)
-    confirmed = db.Column(db.Boolean, nullable=False, default=False)
-    confirmed_on = db.Column(db.DateTime, nullable=True)
-
-    
-    def __init__(self, firstname, lastname, email, password, admin, confirmed, confirmed_on):
-        self.firstname = firstname
-        self.lastname = lastname
-        self.email = email
-        self.password = password
-        self.admin = admin
-        self.confirmed = confirmed
-        self.confirmed_on = confirmed_on
 
 # def create_admin():
 #     """Creates the admin user."""
